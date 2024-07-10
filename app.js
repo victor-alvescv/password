ae62124f5f323a43efa7a3090f8bb523984c6b01
Victor Alves <victoralvescabral7@gmail.com>
2024-07-10 00:27:44 -0300

    main functions working!

--- app.js
+++ app.js
@@ -4,26 +4,50 @@ const second = document.querySelector(".second");
 const third = document.querySelector(".third");
 const fourth = document.querySelector(".fourth");
 let display = document.querySelector(".display");
-let arr = ["-", "-", "-", "-"];
+let arr = [];
+let answer = "1978"
+
 function keyClick(number) {
-    arr.push(number);
-    padChange(number);
-    if (display instanceof HTMLDivElement) {
-        display.innerHTML = `
-  <p class="first">${arr[0]}</p>
-              <p class="second">${arr[1]}</p>
-              <p class="third">${arr[2]}</p>
-              <p class="fourth">${arr[3]}</p>
-  `;
+    if (arr.length < 4) {
+        padChange(number);
+        updateDisplay();
+        console.log(arr);
     }
 }
 function padChange(numberPad) {
-    arr[0] = numberPad;
+    arr.push(numberPad);
 }
-console.log(arr[0]);
 function deleteNumber() {
-    console.log("delete character");
+    arr.pop();
+    updateDisplay();
 }
 function submitNumber() {
-    console.log("submit password");
+    if (arr.join("") === answer) {
+        display.style.color = "#6DDE1A"
+        setTimeout(() => {
+            resetDisplay()
+        }, 2000)
+    }
+    else {
+        display.style.color = "red"
+        setTimeout(() => {
+            resetDisplay()
+        }, 2000)
+    }
 }
+
+function resetDisplay() {
+    display.style.color = "white"
+    arr = [];
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.innerHTML = `
+  <p class="first">${arr[0] || "-"}</p>
+              <p class="second">${arr[1] || "-"}</p>
+              <p class="third">${arr[2] || "-"}</p>
+              <p class="fourth">${arr[3] || "-"}</p>
+  `;
+}
+updateDisplay();
--- app.ts
+++ app.ts
@@ -3,32 +3,44 @@ const second = document.querySelector<HTMLParagraphElement>(".second");
 const third = document.querySelector<HTMLParagraphElement>(".third");
 const fourth = document.querySelector<HTMLParagraphElement>(".fourth");
 
-let display = document.querySelector(".display") as HTMLDivElement | number[];
-let arr: (string | number)[] = ["-", "-", "-", "-"];
+let display = document.querySelector(".display") as HTMLDivElement;
+let arr: (string | number | undefined)[] = [];
 
 function keyClick(number: number): void {
-  arr.push(number);
-  padChange(number);
-  if (display instanceof HTMLDivElement) {
-    display.innerHTML = `
-  <p class="first">${arr[0]}</p>
-              <p class="second">${arr[1]}</p>
-              <p class="third">${arr[2]}</p>
-              <p class="fourth">${arr[3]}</p>
-  `;
+  if (arr.length < 4) {
+    padChange(number);
+      updateDisplay();
+    console.log(arr);
   }
 }
 
 function padChange(numberPad: number): void {
-  arr[0] = numberPad
+  arr.push(numberPad);
 }
 
-console.log(arr[0]);
-
 function deleteNumber() {
-  console.log("delete character")
+  arr.pop();
+  updateDisplay();
 }
 
 function submitNumber() {
-  console.log("submit password")
+  if (arr.join("") === "1978") {
+    alert("correct")
+  } else {
+    arr = []
+    updateDisplay();
+    alert("wrong password")
+  }
+
 }
+
+function updateDisplay() {
+  display.innerHTML = `
+  <p class="first">${arr[0] || "-"}</p>
+              <p class="second">${arr[1] || "-"}</p>
+              <p class="third">${arr[2] || "-"}</p>
+              <p class="fourth">${arr[3] || "-"}</p>
+  `;
+}
+
+updateDisplay();
